4b148ea844090304ee288c9d078d9618f1b9c2e5
zhilinn <52244434+zhilinn@users.noreply.github.com>
2021-01-13 18:04:15 +0300

    remove allowlists

--- src/details/tenant-details.tsx
+++ src/details/tenant-details.tsx
@@ -1,6 +1,6 @@
 import { Component } from '@k8slens/extensions';
 import React from 'react';
-import { Tenant, AllowList } from '../tenant';
+import { Tenant } from '../tenant';
 import './tenant-details.scss';
 
 export type Props = Component.KubeObjectDetailsProps<Tenant>
@@ -24,10 +24,6 @@ export class TenantDetails extends React.Component<Props> {
         <Component.DrawerItem name='Owner kind'>{spec.owner.kind}</Component.DrawerItem>
         <Labels name='Node selector' dict={spec.nodeSelector} />
         <Labels name='Resource Quotas' pairs={resourceQuotas} />
-        <AllowList name='External service IPs' value={spec.externalServiceIPs} />
-        <AllowList name='Container registries' value={spec.containerRegistries} />
-        <AllowList name='Ingress classes' value={spec.ingressClasses} />
-        <AllowList name='Storage classes' value={spec.storageClasses} />
       </div>
     );
   }
@@ -63,22 +59,6 @@ const Labels = (
       {props.values && props.values.map(value => (
         <Component.Badge key={value} label={value} />
       ))}
-    </Component.DrawerItem>
-  );
-};
-
-const AllowList = (props: { name: string, value?: AllowList }) => {
-  if (!props.value)
-    return null;
-
-  return (
-    <Component.DrawerItem name={props.name}>
-      <Labels name='Allowed' values={props.value.allowed} />
-      {props.value.allowedRegex && (
-        <Component.DrawerItem name='Allowed regex'>
-          <code>{props.value.allowedRegex}</code>
-        </Component.DrawerItem>
-      )}
     </Component.DrawerItem>
   );
 };
